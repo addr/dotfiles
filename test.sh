b908b93bf1d50d7287ffb9cc28a59b81302aa7b0
Andy Rice <git@andyrice.io>
2026-02-14 22:49:09 -0500

    Skip install scripts in CI, fix Docker smoke test
    - Add CI guard to brew-bundle and macos-defaults scripts
    - Install curl/git in Docker container before running install.sh

--- test.sh
+++ test.sh
@@ -4,4 +4,7 @@
 # -u: exit on unset variables
 set -eu
 
-docker run --rm -e CI=true -v $(pwd):$(pwd) ubuntu /bin/sh -c "$(pwd)/install.sh"
+docker run --rm -e CI=true -v "$(pwd):$(pwd)" ubuntu /bin/sh -c "
+  apt-get update -qq && apt-get install -y -qq curl git >/dev/null 2>&1
+  $(pwd)/install.sh
+"
